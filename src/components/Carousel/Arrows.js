821d939f80fbd182aa27ea0a58855012594e8564
Kevin Blair <kblair40@gmail.com>
2021-06-23 21:09:04 -0500

    Set centered background position of homepage images

--- src/components/Carousel/Arrows.js
+++ src/components/Carousel/Arrows.js
@@ -1,60 +1,9 @@
 import React from "react";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import IconButton from "@material-ui/core/IconButton";
 
-import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
-
-const useStyles = makeStyles((theme) => ({
-  arrowsContainer: {
-    position: "absolute",
-    bottom: "15%",
-    right: "-12%",
-    // background: "green",
-    zIndex: 50,
-  },
-  forward: {
-    // background: "red",
-    position: "relative",
-  },
-  forwardRoot: {
-    padding: 0,
-    "&:hover": {
-      background: "transparent",
-    },
-  },
-  imageNumber: {
-    // background: "rgba(0,0,0,0.2)",
-    fontFamily: "Oswald",
-    position: "relative",
-    right: "1.5rem",
-  },
-  back: {
-    // background: "purple",
-    position: "relative",
-    right: "3rem",
-  },
-  backwardRoot: {
-    padding: 0,
-    "&:hover": {
-      background: "transparent",
-    },
-  },
-  [theme.breakpoints.down("sm")]: {
-    back: {
-      right: "2.3rem",
-    },
-    imageNumber: {
-      right: "1.15rem",
-    },
-  },
-  [theme.breakpoints.down("xs")]: {
-    arrowsContainer: {
-      display: "none",
-    },
-  },
-}));
+import { useStyles } from "../../styles/ArrowsStyles";
 
 const Arrows = ({ nextSlide, prevSlide }) => {
   const classes = useStyles();
--- src/components/Carousel/ImageCarousel.js
+++ src/components/Carousel/ImageCarousel.js
@@ -4,13 +4,42 @@ import "react-slideshow-image/dist/styles.css";
 
 import CarouselTextOverlay from "./CarouselTextOverlay";
 import Arrows from "./Arrows";
-import six from "../../assets/images/six.jpg";
+import six_lg from "../../assets/images/six_lg.jpg";
+import six_sm from "../../assets/images/six_sm.jpg";
 import seven from "../../assets/images/seven.jpg";
-import eight from "../../assets/images/eight.jpg";
-import { useStyles } from "../../styles/ImageCarouselStyles";
+import eight_sm from "../../assets/images/eight_sm.jpg";
+import eight_lg from "../../assets/images/eight_lg.jpg";
+import eight from "../../assets/images/eight.jpeg";
+// import { useStyles } from "../../styles/ImageCarouselStyles";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+  carouselContainer: {
+    position: "relative",
+    left: "-6%",
+    marginTop: "3.5rem",
+    width: "86%",
+  },
+  singleSlide: {
+    "& > div": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "55vh",
+    },
+  },
+  [theme.breakpoints.down("xs")]: {
+    carouselContainer: {
+      width: "98%",
+      left: "-1%",
+    },
+  },
+}));
 
 const overlayTexts = ["Jackets", "Winter Coats", "Summer Collection"];
-const slideImages = [six, seven, eight];
+const slideImages = [six_sm, seven, eight_sm];
 
 const MainCarousel = () => {
   const slideRef = useRef();
