8c3bfab67e7b86e439341c874d0117da8ad97b10
Kevin Blair <kblair40@gmail.com>
2021-06-30 13:45:38 -0500

    Add better jewelry and electronics images

--- src/constants.js
+++ src/constants.js
@@ -55,12 +55,13 @@ export const PRODUCTS = [
   },
   {
     id: "VNGBV587",
-    title: "Solid Gold Petite Micropave",
+    title: "Solid Gold Petite Necklace",
     price: 168,
     description:
       "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
     category: "jewelry",
-    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    // image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    image: "https://i.imgur.com/W5addzp.jpg",
     needsSizes: false,
   },
   {
@@ -75,12 +76,13 @@ export const PRODUCTS = [
   },
   {
     id: "QWKOTR845",
-    title: "Pierced Gold Plated Double",
+    title: "Pierced Silver Plated Double",
     price: 10.99,
     description:
-      "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+      "Silver Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
     category: "jewelry",
-    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    // image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    image: "https://i.imgur.com/nq8dLvj.jpg",
     needsSizes: false,
   },
   {
@@ -95,12 +97,13 @@ export const PRODUCTS = [
   },
   {
     id: "QKBUC587",
-    title: "SanDisk 1TB Internal SSD - 6 Gb/s",
+    title: "WesternDigital 1TB Internal HHD - 6 Gb/s",
     price: 109,
     description:
       "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+    // image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+    image: "https://i.imgur.com/CwUJqVB.jpg",
     needsSizes: false,
   },
   {
